e113831f641e949c90895828a18bf5282c31228b
Samp83 <sami.paris@supdevinci-edu.fr>
2025-02-23 18:17:34 +0100

    added checkpoints

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class BallManager : MonoBehaviour
 {
     private Vector3 startPosition;
+    private Vector3 lastCheckpoint;
 
     void Start()
     {
         startPosition = transform.position;
+        lastCheckpoint = transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,14 +20,46 @@ public class BallManager : MonoBehaviour
         else if (other.CompareTag("Goal"))
         {
             Debug.Log("Victoire !");
-            
+            RespawnBallStart(); 
+            ResetPlatformRotation();
+            //ShowWinScreen();
+        }
+        else if (other.CompareTag("Checkpoint"))
+        {
+            lastCheckpoint = other.transform.position;
         }
     }
 
     void RespawnBall()
     {
+        transform.position = lastCheckpoint;
+        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+
+    void RespawnBallStart()
+    {
         transform.position = startPosition;
         GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
+
+    void Update()
+    {
+        if (transform.position.y < -5) 
+        {
+            RespawnBall();
+            ResetPlatformRotation();
+        }
+    }
+
+    void ResetPlatformRotation()
+    {
+        GameObject platform = GameObject.Find("Platform");
+        if (platform != null)
+        {
+            platform.transform.rotation = Quaternion.identity;
+        }
+    }
+
 }
